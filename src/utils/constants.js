4e49ddc5ee599b2ed7a1fab918906f6736d3487e
Chetan Sahu <sahu5chetan5@gmail.com>
2025-05-24 08:51:22 +0530

    fix for 401 route error

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,7 @@
+import axios from 'axios';
+
 export const BASE_URL = "https://dev-tinder-ffpk.onrender.com"
+
+// Configure axios defaults
+axios.defaults.withCredentials = true;
+axios.defaults.headers.common['Content-Type'] = 'application/json';
